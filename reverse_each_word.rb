5176ba807b29d6975d8ec0159af416984e59e850
Dan Kagan <dankagan7@gmail.com>
2019-04-04 00:24:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,12 +7,3 @@ def reverse_each_word(string)
     end
     return storage.join(" ")
 end
-
-
-#convert string to array of strings
-#reverse the letters of EACH string
-
-
-  #turn string into a an array
-  #reverse the letters in a NEW string, not the order of words
-  #use .collect
